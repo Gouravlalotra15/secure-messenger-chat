fdb18ff5e44821437de21cd31d0469673734a10d
Gourav <gouravlalotraa@gmail.com>
2023-03-27 22:14:58 +0530

    added the build folder for client

--- server.js
+++ server.js
@@ -8,15 +8,15 @@ const { Server } = require("socket.io");
 const { addUser, getUsersInRoom, removeUser } = require("./users");
 
 const SECRET_KEY = process.env.SECRET_KEY || "iamthere";
-const homeDir = path.join(__dirname, "client", "build");
+const homeDir = path.join(__dirname, "client", "build");//cross platform function :- this will return you the current file location
+//path.join:- because of path directory have different configuration wrt o.s eg linux or windows we use this fxn 
 
 console.log(homeDir);
-
 app.use(cors());
-app.use(express.static(homeDir));
+app.use(express.static(homeDir));// use this path to serve static assest(used in prodeuction only)
 
 const port = process.env.PORT || 5000;
-const server = http.createServer(app);
+const server = http.createServer(app);//intializing server
 
 const io = new Server(server, {
   cors: {
@@ -29,7 +29,9 @@ const io = new Server(server, {
 // const io = new Server(server);
 
 io.on("connection", (socket) => {
-  console.log(`User Connected: ${socket.id}`);
+  console.log(`User Connected: ${socket.id}`);//this sxn will start when there a connewction bulit in server and frontend
+
+  // join room is a click event ...here it will receive the event and fnx will be called
 
   socket.on("join_room", (data, cb) => {
     const user = addUser(socket.id, data.username, data.roomname);
@@ -51,12 +53,20 @@ io.on("connection", (socket) => {
 
     // updating the user count in the room
     const totalUsers = getUsersInRoom(data.roomname);
+    // io. mein events gobally associated hai
     io.to(data.roomname).emit("meta_info", {
       totalActiveUsers: totalUsers.length,
       secretKey: SECRET_KEY,
-    });
+    });//data private rakh rha hai......ek group ki info dusre mein forward nhi krr rha
+
+
 
     // send the message to the other users
+
+
+    // socket. broadcast ho rha hai but not the action creater
+
+    //.to :- event emit krna
     socket.to(data.roomname).emit("receive_message", {
       uid: new Date().getMilliseconds(),
       message: `${data.username}, has joined us 🎊`,
@@ -64,6 +74,8 @@ io.on("connection", (socket) => {
     });
   });
 
+
+  //.on :- lsitening to the event with specific event
   socket.on("send_message", (data) => {
     // console.log(data);
     // here should the encryption run
