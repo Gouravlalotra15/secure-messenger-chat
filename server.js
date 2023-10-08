74950d64f6fe51972c39a89602d53e4ff14d303f
Gouravlalotra15 <74850322+Gouravlalotra15@users.noreply.github.com>
2023-10-08 19:43:39 +0530

    Update server.js

--- server.js
+++ server.js
@@ -20,7 +20,7 @@ const server = http.createServer(app);//intializing server
 
 const io = new Server(server, {
   cors: {
-    origin: "http://localhost:3000",
+    origin: "https://secure-chat-messenger.onrender.com/",
     methods: ["GET", "POST"],
   },
 });
